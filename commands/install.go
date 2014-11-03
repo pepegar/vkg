a7706f7c3166e8af9a33545d01cb96fa193e1296
Pepe García <joseluisgarcia@packlink.es>
2014-11-03 01:45:47 +0100

    refactor(install.go): refactor action install
    split megamoth action into separate functions

--- commands/install.go
+++ commands/install.go
@@ -25,67 +25,73 @@ func IsGithubUrl(path string) bool {
 	return match
 }
 
-func IsVimawesomeSlug(param string) bool {
-	return (!IsGithubUrl(param) && !IsUserRepo(param))
+func IsVimawesomeSlug(plugin string) bool {
+	return (!IsGithubUrl(plugin) && !IsUserRepo(plugin))
+}
+
+func installAllVkgrcPlugins() {
+	vkgConfig := config.GetVkgGonfig()
+	vkgrcContents, err := ioutil.ReadFile(vkgConfig.VkgrcPath)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	vkgrc := vkgrc.ParseVkgrc(vkgrcContents)
+
+	for _, plugin := range vkgrc.Plugins {
+		if err := utils.Git.Clone(plugin.Repository, plugin.Branch); err == nil {
+			fmt.Printf(vkgConfig.Messages["successfully_installed"], plugin.Repository)
+		} else {
+			fmt.Printf(vkgConfig.Messages["plugin_already_installed"], plugin.Repository)
+		}
+	}
+}
+
+func installSinglePlugin(plugin string) {
+	vkgConfig := config.GetVkgGonfig()
+	var slug string
+	var url string
+
+	if IsUserRepo(plugin) {
+		parts := strings.Split(plugin, "/")
+		slug = parts[len(parts)-1]
+		url = "https://github.com/" + plugin
+	} else if IsGithubUrl(plugin) {
+		parts := strings.Split(plugin, "/")
+		slug = parts[len(parts)-1]
+		url = "https://" + plugin
+	} else if IsVimawesomeSlug(plugin) {
+		jsonUrl := fmt.Sprintf(vkgConfig.VimawesomePluginUrl, plugin)
+		body, requestError := GetJson(jsonUrl)
+
+		if requestError != nil {
+			log.Fatal(vkgConfig.Messages["request_error"])
+		} else {
+			plugin, parseError := ParseSinglePlugin(body)
+
+			if parseError != nil {
+				log.Fatal(parseError)
+			} else {
+				url = plugin.GithubUrl
+				slug = plugin.Slug
+			}
+		}
+	}
+
+	if err := utils.Git.Clone(url, "master"); err == nil {
+		fmt.Printf(vkgConfig.Messages["successfully_installed"], slug)
+	} else {
+		fmt.Println(err)
+	}
 }
 
 func actionInstall() {
-	vkgConfig := config.GetVkgGonfig()
-	if len(os.Args) < 3 {
-		if config.VkgrcExists() {
-			vkgrcContents, err := ioutil.ReadFile(vkgConfig.VkgrcPath)
-
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			vkgrc := vkgrc.ParseVkgrc(vkgrcContents)
-
-			for _, plugin := range vkgrc.Plugins {
-				if err := utils.Git.Clone(plugin.Repository, plugin.Branch); err == nil {
-					fmt.Printf(vkgConfig.Messages["successfully_installed"], plugin.Repository)
-				} else {
-					fmt.Printf(vkgConfig.Messages["plugin_already_installed"], plugin.Repository)
-				}
-			}
-		}
+	if len(os.Args) < 3 && config.VkgrcExists() {
+		installAllVkgrcPlugins()
 	} else {
-		var slug string
-		var url string
-
-		param := os.Args[2]
-
-		if IsUserRepo(param) {
-			parts := strings.Split(param, "/")
-			slug = parts[len(parts)-1]
-			url = "https://github.com/" + param
-		} else if IsGithubUrl(param) {
-			parts := strings.Split(param, "/")
-			slug = parts[len(parts)-1]
-			url = "https://" + param
-		} else if IsVimawesomeSlug(param) {
-			jsonUrl := fmt.Sprintf(vkgConfig.VimawesomePluginUrl, param)
-			body, requestError := GetJson(jsonUrl)
-
-			if requestError != nil {
-				log.Fatal(vkgConfig.Messages["request_error"])
-			} else {
-				plugin, parseError := ParseSinglePlugin(body)
-
-				if parseError != nil {
-					log.Fatal(parseError)
-				} else {
-					url = plugin.GithubUrl
-					slug = plugin.Slug
-				}
-			}
-		}
-
-		if err := utils.Git.Clone(url, "master"); err == nil {
-			fmt.Printf(vkgConfig.Messages["successfully_installed"], slug)
-		} else {
-			fmt.Println(err)
-		}
+		plugin := os.Args[2]
+		installSinglePlugin(plugin)
 	}
 }
 
