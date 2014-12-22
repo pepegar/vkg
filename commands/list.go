dff00d52d024fda2d3331961bad6a6c024ee4a4e
Pepe García <joseluisgarcia@packlink.es>
2014-12-23 00:19:02 +0100

    feat(list): refactor the list command a bit

--- commands/list.go
+++ commands/list.go
@@ -3,21 +3,41 @@ package commands
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
+	"strings"
 
 	"github.com/pepegar/vkg/config"
 )
+
+func getPlugins() []os.FileInfo {
+	config := config.GetVkgGonfig()
+
+	files, _ := ioutil.ReadDir(config.PluginsPath)
+
+	return files
+}
+
+func getListOutput(files []os.FileInfo) string {
+	var result string
+
+	for _, file := range files {
+		result += "* " + file.Name() + "\n"
+	}
+
+	return strings.TrimSpace(result)
+}
+
+func action() {
+	files := getPlugins()
+
+	output := getListOutput(files)
+
+	fmt.Println(output)
+}
 
 var ListCommand = Command{
 	Name:        "list",
 	Description: "list all installed packages",
 	Usage:       "list",
-	Action: func() {
-		config := config.GetVkgGonfig()
-
-		files, _ := ioutil.ReadDir(config.PluginsPath)
-
-		for _, file := range files {
-			fmt.Println("* " + file.Name())
-		}
-	},
+	Action:      action,
 }
